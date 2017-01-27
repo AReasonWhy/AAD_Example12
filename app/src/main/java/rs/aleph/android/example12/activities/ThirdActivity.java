0a8ae4259c12d0a4d51b822e6949d632e904273f
AReasonWhy <pharotek.jesod@gmail.com>
2017-01-27 20:21:37 +0100

    changes to third activity

--- app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
+++ app/src/main/java/rs/aleph/android/example12/activities/ThirdActivity.java
@@ -1,8 +1,23 @@
 package rs.aleph.android.example12.activities;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Pharotek on 27-Jan-17.
  */
 
-public class ThirdActivity {
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
 }
